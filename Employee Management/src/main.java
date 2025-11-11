5a59bff0d9daea49620567e97ba613da722d85ea
Chris Barber <christopherbarber4@googlemail.com>
2025-11-11 16:28:44 +0000

    "Added method"

--- Employee Management/src/main.java
+++ Employee Management/src/main.java
@@ -5,7 +5,12 @@ public class main {
 		//added in some extra comments
 		
 		System.out.println("Hello World");
+		message();
 
+	}
+	
+	public static void message() {
+		System.out.println("This is a message");
 	}
 
 }
