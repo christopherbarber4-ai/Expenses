5aee4fc9db82b9838bf38d04adf76e8ff03515c0
Chris Barber <christopherbarber4@googlemail.com>
2025-11-11 16:27:21 +0000

    Added main method

--- Employee Management/src/main.java
+++ Employee Management/src/main.java
@@ -2,7 +2,9 @@
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//added in some extra comments
+		
+		System.out.println("Hello World");
 
 	}
 
